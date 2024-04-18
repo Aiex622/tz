a3d2bb6d844c587a8033b44a6b3253dc1b6fd8d8
Aiex <unrealexplorer@gmail.com>
2024-04-18 17:42:28 +0300

    it works

--- tz1.sh
+++ tz1.sh
@@ -3,7 +3,6 @@ if [ $# -ne 2 ]; then
   echo "Необходимо указать две директории"
   exit 1
 fi
-
 src_dir="$1"
 dst_dir="$2"
 if [ ! -d "$src_dir" ]; then
@@ -16,14 +15,22 @@ fi
 function copy_files {
   local src_dir="$1"
   local dst_dir="$2"
+  local count=1
   for entry in "$src_dir"/*; do
     if [ -f "$entry" ]; then
-      cp "$entry" "$dst_dir"
+      filename=$(basename "$entry")
+      extension="${filename##*.}"
+      filename="${filename%.*}"
+      new_filename="${filename}"
+      while [ -f "${dst_dir}/${new_filename}.${extension}" ]; do
+        count=$((count + 1))
+        new_filename="${filename}_${count}"
+      done
+      cp "$entry" "${dst_dir}/${new_filename}.${extension}"
     elif [ -d "$entry" ]; then
       copy_files "$entry" "$dst_dir"
     fi
   done
 }
 copy_files "$src_dir" "$dst_dir"
-
 echo "Копирование файлов из '$src_dir' в '$dst_dir' завершено."
